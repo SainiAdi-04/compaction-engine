9d21a7ef18f8294b366b977f2cbb3719b05bacb3
ad <sainiadi03@gmail.com>
2026-08-26 11:10:54 +0530

    sessionLog: stores the list of entries in order

---
+++ src/session/sessionLog.ts
@@ -0,0 +1,23 @@
+import { SessionEntry } from "../types.ts";
+
+export class SessionLog {
+    private entries: SessionEntry[] = [];
+
+    addEntry(entry:SessionEntry):void{
+        this.entries.push(entry);
+    }
+
+    getAllEntries():SessionEntry[]{
+        return this.entries;
+    }
+
+    getEntriesfromId(id:string):SessionEntry[]{
+        const index = this.entries.findIndex(entry=>entry.id===id);
+
+        if(index===-1){
+            throw new Error(`SessionLog: no entry found with id "${id}"`);
+        }
+
+        return this.entries.slice(index)
+    }
+}
